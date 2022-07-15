e5a41e19d962cb6939e8ba6e0b6946fe1b3380ef
nathanvr <jonathanvaro1004@gmail.com>
2022-07-14 21:49:27 -0500

    Adds the model of product

---
+++ src/models/product.model.js
@@ -0,0 +1,61 @@
+const { Schema, model, models } = require("mongoose");
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      enum: {
+        values: [
+          "vegetarian",
+          "vegan",
+          "fastfood",
+          "fitness",
+          "italian",
+          "seafood",
+          "regional",
+        ],
+        message: "Wrong Category",
+      },
+    },
+
+    priceUnit: {
+      type: Number,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    maxQty: {
+      type: Number,
+      required: true,
+    },
+
+    available: {
+      type: Boolean,
+      required: true,
+    },
+
+    userId: { type: Schema.Types.ObjectId, ref: "User", require: true },
+
+    productCars: {
+      type: [{ type: Schema.Types.ObjectId, ref: "ProductCar" }],
+      required: true,
+    },
+  },
+
+  { timestamps: true }
+);
